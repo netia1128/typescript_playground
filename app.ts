4ab4a8ff00bf1c10c6d500149a98c2da5bad1ebd
Ingram <netia.ingram@charter.com>
2023-06-21 22:13:15 -0600

    chore : Add more TypeScript sample code

--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
+type exampleUnionType = number | string
+
 function add(num1: number , num2: number) {
   return num1 + num2;
 }
 
 console.log(add(2, 4));
+
+function addAnything(input1: exampleUnionType, input2: exampleUnionType) {
+  return `${input1} and ${input2}`;
+}
+
+console.log(addAnything(2, 4));
+console.log(addAnything("Hello", "World"));
 
 const pet: {
   type: string;
@@ -19,3 +28,7 @@ console.log(pet);
 const foods: string[] = [`pickles`, `chips`, `tacos`];
 
 console.log(foods);
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }
+
+console.log(Role.ADMIN);
