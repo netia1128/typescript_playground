731f7a701a8dc588da483cbe689d2e3d4ecca823
Ingram <netia.ingram@charter.com>
2023-06-21 21:33:13 -0600

    chore : Add type declaration examples

--- app.ts
+++ app.ts
@@ -1,1 +1,21 @@
-console.log('Hello World');
+function add(num1: number , num2: number) {
+  return num1 + num2;
+}
+
+console.log(add(2, 4));
+
+const pet: {
+  type: string;
+  name: string;
+  age: number;
+} = {
+  type: `cat`,
+  name: `Gow`,
+  age: 18
+}
+
+console.log(pet);
+
+const foods: string[] = [`pickles`, `chips`, `tacos`];
+
+console.log(foods);
